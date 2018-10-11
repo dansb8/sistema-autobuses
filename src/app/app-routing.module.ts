fae1f98a0c677188486d7e8ad25617621421797a
Fermin <danielromo8@hotmail.com>
2018-10-11 13:22:13 -0500

    rxjs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,21 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from './components/home/home.component'
 import {UserComponent} from './components/user/user.component'
 import {TicketComponent} from './components/user/ticket/ticket.component'
+import {PersonalComponent} from './components/user/personal/personal.component'
+import {ReportComponent} from './components/user/report/report.component'
+import {CreditComponent} from './components/user/credit/credit.component'
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'user', component: UserComponent },
-  { path: 'user/ticket', component: TicketComponent },
+  { 
+    path: 'user', component: UserComponent,
+    children: [
+      { path: 'ticket' , component: TicketComponent},
+      { path: 'personal' , component: PersonalComponent},
+      { path: 'report' , component: ReportComponent},
+      { path: 'cards' , component: CreditComponent},
+      { path: '' ,redirectTo: 'ticket', pathMatch: 'full'}
+    ] 
+  },
   {path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
 
