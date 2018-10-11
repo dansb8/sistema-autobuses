e3b697d92a055796fb017920e399730e7614d346
Fermin <danielromo8@hotmail.com>
2018-10-10 22:47:43 -0500

    estructura general

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from './components/home/home.component'
+import {UserComponent} from './components/user/user.component'
+import {TicketComponent} from './components/user/ticket/ticket.component'
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'user', component: UserComponent },
+  { path: 'user/ticket', component: TicketComponent },
   {path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,43 @@ import { FooterComponent } from './components/shared/footer/footer.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './components/home/home.component';
+import { UserComponent } from './components/user/user.component';
+import { TicketComponent } from './components/user/ticket/ticket.component';
+import { ReportComponent } from './components/user/report/report.component';
+import { PersonalComponent } from './components/user/personal/personal.component';
+import { CreditComponent } from './components/user/credit/credit.component';
+import { SearchComponent } from './components/search/search.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DirectoryComponent } from './components/directory/directory.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { BusComponent } from './components/admin/bus/bus.component';
+import { TerminalComponent } from './components/admin/terminal/terminal.component';
+import { RoutesComponent } from './components/admin/routes/routes.component';
+import { UsersComponent } from './components/admin/users/users.component';
+import { TicketsComponent } from './components/admin/tickets/tickets.component';
 //rutas
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    UserComponent,
+    TicketComponent,
+    ReportComponent,
+    PersonalComponent,
+    CreditComponent,
+    SearchComponent,
+    LoginComponent,
+    RegisterComponent,
+    DirectoryComponent,
+    AdminComponent,
+    BusComponent,
+    TerminalComponent,
+    RoutesComponent,
+    UsersComponent,
+    TicketsComponent
   ],
   imports: [
     BrowserModule,
