5616a76d22262c762f7e2539902e707b82fd1d72
VictorVidales <victor_vidales97@hotmail.com>
2018-10-11 15:38:40 -0500

    Connection to Node

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from './components/home/home.component'
+import {DirectoryComponent} from './components/directory/directory.component'
+import {SearchComponent} from './components/search/search.component'
 import {UserComponent} from './components/user/user.component'
 import {TicketComponent} from './components/user/ticket/ticket.component'
 import {PersonalComponent} from './components/user/personal/personal.component'
@@ -9,6 +11,8 @@ import {ReportComponent} from './components/user/report/report.component'
 import {CreditComponent} from './components/user/credit/credit.component'
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'directorio', component: DirectoryComponent},
+  { path: 'busqueda', component: SearchComponent},
   { 
     path: 'user', component: UserComponent,
     children: [
