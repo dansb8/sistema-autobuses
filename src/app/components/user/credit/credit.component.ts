01836337ad4a1fd8bb6636e8a54cb7b6b4ed7ae2
Jesus Alonso <papanaton123@hotmail.com>
2018-10-15 17:38:59 -0500

    WIP

--- src/app/components/user/credit/credit.component.ts
+++ src/app/components/user/credit/credit.component.ts
@@ -6,6 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./credit.component.css']
 })
 export class CreditComponent implements OnInit {
+  public numCards = 3;
+  public cards = [
+    {
+      'user': 'Victor Manuel',
+      'numbers': '1234567890001234',
+      'nip': '1234',
+      'company': 'visa',
+      'expiration': '01/20'
+    },
+  {
+      'user': 'Daniel Fermin',
+      'numbers': '1234567567001234',
+      'nip': '3434',
+      'company': 'visa',
+      'expiration': '05/20'
+  },
+  {
+      'user': 'Carlos Alberto',
+      'numbers': '1234567567029834',
+      'nip': '3420',
+      'company': 'masterCard',
+      'expiration': '11/20'
+  }];
 
   constructor() { }
 
