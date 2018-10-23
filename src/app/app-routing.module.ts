7a61290c8c18120d5ab531bf679474488e4f7d05
Jesus Alonso <papanaton123@hotmail.com>
2018-10-23 09:42:49 -0500

    WIP
    Finalizadas: Tarjetas de credito, home, registro,login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,31 @@
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from './components/home/home.component'
-import {DirectoryComponent} from './components/directory/directory.component'
-import {SearchComponent} from './components/search/search.component'
-import {UserComponent} from './components/user/user.component'
-import {TicketComponent} from './components/user/ticket/ticket.component'
-import {PersonalComponent} from './components/user/personal/personal.component'
-import {ReportComponent} from './components/user/report/report.component'
-import {CreditComponent} from './components/user/credit/credit.component'
+import {HomeComponent} from './components/home/home.component';
+import {DirectoryComponent} from './components/directory/directory.component';
+import {SearchComponent} from './components/search/search.component';
+import {UserComponent} from './components/user/user.component';
+import {TicketComponent} from './components/user/ticket/ticket.component';
+import {PersonalComponent} from './components/user/personal/personal.component';
+import {ReportComponent} from './components/user/report/report.component';
+import {CreditComponent} from './components/user/credit/credit.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'directorio', component: DirectoryComponent},
   { path: 'busqueda', component: SearchComponent},
-  { 
+  { path: 'ingresar', component: LoginComponent},
+  { path: 'registrarse', component: RegisterComponent},
+  {
     path: 'user', component: UserComponent,
     children: [
       { path: 'ticket' , component: TicketComponent},
       { path: 'personal' , component: PersonalComponent},
       { path: 'report' , component: ReportComponent},
       { path: 'cards' , component: CreditComponent},
-      { path: '' ,redirectTo: 'ticket', pathMatch: 'full'}
-    ] 
+      { path: '' , redirectTo: 'ticket', pathMatch: 'full'}
+    ]
   },
   {path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
--- src/app/components/user/credit/credit.component.ts
+++ src/app/components/user/credit/credit.component.ts
@@ -10,25 +10,46 @@ export class CreditComponent implements OnInit {
   public cards = [
     {
       'user': 'Victor Manuel',
-      'numbers': '1234567890001234',
+      'numbers': '1234-5678-9000-1234',
       'nip': '1234',
-      'company': 'visa',
+      'company': 'Visa',
       'expiration': '01/20'
     },
   {
       'user': 'Daniel Fermin',
-      'numbers': '1234567567001234',
+      'numbers': '1234-5675-6700-1234',
       'nip': '3434',
-      'company': 'visa',
+      'company': 'Visa',
       'expiration': '05/20'
   },
   {
       'user': 'Carlos Alberto',
-      'numbers': '1234567567029834',
+      'numbers': '1234-5675-6702-9834',
       'nip': '3420',
-      'company': 'masterCard',
+      'company': 'MasterCard',
       'expiration': '11/20'
-  }];
+  },
+  {
+    'user': 'Carlos Alberto',
+    'numbers': '1234-5675-6702-9834',
+    'nip': '3420',
+    'company': 'MasterCard',
+    'expiration': '11/20'
+},
+{
+  'user': 'Carlos Alberto',
+  'numbers': '1234-5675-6702-9834',
+  'nip': '3420',
+  'company': 'MasterCard',
+  'expiration': '11/20'
+},
+{
+  'user': 'Carlos Alberto',
+  'numbers': '1234-5675-6702-9834',
+  'nip': '3420',
+  'company': 'MasterCard',
+  'expiration': '11/20'
+}];
 
   constructor() { }
 
