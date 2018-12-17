06ccf3011c0cfa06eadc6d50519eef91f688d589
Fermin <danielromo8@hotmail.com>
2018-12-17 10:10:46 -0600

    tickets-report

--- src/app/components/user/report/report.component.ts
+++ src/app/components/user/report/report.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-
-@Component({
+import { UserService } from 'src/app/services/user.service';
+import { AuthService} from 'src/app/services/auth.service';
+import { ticket } from 'src/app/interfaces/ticket';
+@Component({  
   selector: 'app-report',
   templateUrl: './report.component.html',
   styleUrls: ['./report.component.css']
 })
 export class ReportComponent implements OnInit {
 
-  constructor() { }
+  Purchase: any [];
+
+
+
+  constructor(private userservice: UserService,private authService: AuthService) {
+   
+   }
 
   ngOnInit() {
+    if(this.authService.prueba){
+      this.Purchase=[{
+        date:"12-05-18",
+        schedule:"10:00",
+        origin: "AGS",
+        destination: "CAL",
+        passengers: [{
+          name:"Daniel Romo",
+
+        }],
+        card:{
+          name: "Daniel Romo",
+          num:"XXXX-XXXX-XXXX-1234"
+        }
+
+      }]
+    }
   }
 
 }
