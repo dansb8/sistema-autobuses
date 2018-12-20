2982429a86c4fe8f7696b0a66a73182e6c802ee8
Jesus Alonso <papanaton123@hotmail.com>
2018-12-20 00:08:57 -0600

    sidebar actualizado

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $: any; // para jquery
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
@@ -10,6 +10,35 @@ export class AdminComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    jQuery(function ( $ ) {
+      $('.sidebar-dropdown > a').click(function() {
+    $('.sidebar-submenu').slideUp(200);
+    if (
+      $(this)
+        .parent()
+        .hasClass('active')
+    ) {
+      $('.sidebar-dropdown').removeClass('active');
+      $(this)
+        .parent()
+        .removeClass('active');
+    } else {
+      $('.sidebar-dropdown').removeClass('active');
+      $(this)
+        .next('.sidebar-submenu')
+        .slideDown(200);
+      $(this)
+        .parent()
+        .addClass('active');
+    }
+  });
+  $('#close-sidebar').click(function() {
+    $('.page-wrapper').removeClass('toggled');
+  });
+  $('#show-sidebar').click(function() {
+    $('.page-wrapper').addClass('toggled');
+  });
+  });
   }
 
 }
--- src/app/components/user/edit-card/edit-card.component.ts
+++ src/app/components/user/edit-card/edit-card.component.ts
@@ -10,38 +10,39 @@ declare var $: any; // para jquery
 })
 export class EditCardComponent implements OnInit {
   public flagToggle =  true;
-  name: string
-  cardnum: string
-  expmonth: number
-  expyear:number
-  cvc: number
-  company: string
-  type:number
-  alert:boolean
-  constructor(private userService:UserService,private authService: AuthService) {}
+  name: string;
+  cardnum: string;
+  expmonth: number;
+  expyear: number;
+  cvc: number;
+  company: string;
+  type: number;
+  alert: boolean;
+  constructor(private userService: UserService, private authService: AuthService) {}
 
   ngOnInit() {
     this.alert = false;
   }
-  addCard(cardform: NgForm){
-    if(!this.authService.prueba){
+  addCard(cardform: NgForm) {
+    if (!this.authService.prueba) {
       console.log(this.cardnum);
-      
-    if(cardform && cardform.valid){
+    if (cardform && cardform.valid) {
       this.userService.addcard(this.name,this.type,this.company,this.authService.currentUser.id,this.expmonth,this.expyear,this.cardnum,this.cvc).subscribe((res:boolean)=>{
-        if(res){
-         this.alert=true;
+        if (res) {
+         this.alert = true;
         }
       });
     }
-  }else{
-    this.alert=true;
+  } else {
+    this.alert = true;
   }
   }
-  format(event: KeyboardEvent){
-    if(event.key!='Backspace')
-    if(this.cardnum.length==4 ||this.cardnum.length==9||this.cardnum.length==14)
-      this.cardnum+="-"
+  format(event: KeyboardEvent) {
+    if (event.key !== 'Backspace') {
+      if (this.cardnum.length === 4 || this.cardnum.length === 9 || this.cardnum.length === 14) {
+        this.cardnum += '-';
+    }
+    }
   }
 
 }
